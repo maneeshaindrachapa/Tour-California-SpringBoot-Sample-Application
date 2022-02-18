319f7102e68bf75a19cde780ad2c3f0ee33cf011
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2022-02-18 21:29:15 +0530

    Main application.

---
+++ src/test/java/com/explore/california/CaliforniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explore.california;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaliforniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
